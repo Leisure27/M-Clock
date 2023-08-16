9306c4f6ada8808f702c14ef03604f8d6facc71f
稚子 <1973688584@qq.com>
2023-08-16 16:18:07 +0800

    时间和日期显示设计完成，接下来接入和风天气获取天气信息

--- 1.M-Clock/src/main.cpp
+++ 1.M-Clock/src/main.cpp
@@ -2,6 +2,8 @@
 #include <TFT_eSPI.h>
 #include "M_Config.h"
 #include <NTP.h>
+#include <string.h>
+#include "KaiTI_30.h" // 自定义字体
 
 #ifdef ESP8266
 #include <ESP8266WiFi.h>
@@ -20,6 +22,7 @@ TFT_eSprite clk = TFT_eSprite(&tft);
 
 /*函数声明区*/
 String num2str(uint16_t num);
+uint8_t Zeller_Week();
 
 /*自定义函数区*/
 void Connect_Wifi()
@@ -52,7 +55,7 @@ void TFT_Init()
   analogWrite(BLK, TFT_Light);
 }
 
-// 数字转化为字符串
+// 数字转化为字符串，将时间以两位数显示
 String num2str(uint16_t num)
 {
   String s = "";
@@ -65,43 +68,78 @@ String num2str(uint16_t num)
 }
 
 // 将时分秒组成字符串
-String Time_s()
+String Time_str()
 {
   String s = num2str(Current_Time.hour) + ":" + num2str(Current_Time.minute) + ":" + num2str(Current_Time.second);
   return s;
+}
+
+String Week_str()
+{
+  uint8_t w = Zeller_Week();
+  String week[7] = {"日", "一", "二", "三", "四", "五", "六"};
+  String s = "周" + week[w];
+  return s;
+}
+
+// 蔡勒(Zeller)公式：快速将任意日期转换为星期数
+uint8_t Zeller_Week()
+{
+  uint16_t year = Current_Time.year;
+  uint8_t month = Current_Time.month;
+  uint8_t day = Current_Time.day;
+  if (month < 3)
+  {
+    year--;
+    month += 12;
+  }
+  int c = int(year / 100), y = year - 100 * c;
+  int w = int(c / 4) - 2 * c + y + int(y / 4) + (26 * (month + 1) / 10) + day - 1;
+  w = (w % 7 + 7) % 7; // 0：Sunday，1:Monday
+  return w;
 }
 
 void NTP_Time()
 {
 
   Current_Time = NTPGetTime();
-  Serial.print("Current time: ");
-  Serial.print(Current_Time.year);
-  Serial.print("/");
-  Serial.print(Current_Time.month);
-  Serial.print("/");
-  Serial.print(Current_Time.day);
-  Serial.print(" ");
-  Serial.print(Current_Time.hour);
-  Serial.print(":");
-  Serial.print(Current_Time.minute);
-  Serial.print(":");
-  Serial.println(Current_Time.second);
+
+  clk.setColorDepth(8);
 
   // 显示当前时间
-  clk.setColorDepth(8);
   clk.createSprite(220, 48);   // 创建一个画布，画布大小x*y
   clk.fillSprite(TFT_BLACK);   // 画布颜色
   clk.setTextColor(TFT_WHITE); // 字体颜色
   clk.setTextFont(7);          // 字体类型Font 7，字体大小：48*48
   clk.setTextDatum(TL_DATUM);  // 设置文本显示基准：左上对齐(默认对齐方式)
-  clk.drawString(Time_s(), 0, 0);
-
-  clk.pushSprite(2, 2); // 把画布推送到屏幕的(x,y)位置进行显示
-  clk.unloadFont();     // 卸载字体
-  clk.deleteSprite();   // 删除画布
+  clk.drawString(Time_str(), 0, 0);
+  clk.pushSprite(2, 38); // 把画布推送到屏幕的(x,y)位置进行显示
+  clk.unloadFont();      // 卸载字体
+  clk.deleteSprite();    // 删除画布
 
   // 显示日期
+  clk.createSprite(120, 32);   // 创建一个画布，画布大小x*y
+  clk.fillSprite(TFT_BLACK);   // 画布颜色
+  clk.setTextColor(TFT_WHITE);    // 字体颜色
+  clk.loadFont(KaiTI_30);      // 加载字体
+  clk.setTextDatum(TL_DATUM);  // 设置文本显示基准：左上对齐(默认对齐方式)
+  clk.print(Current_Time.month);
+  clk.print("月");
+  clk.print(Current_Time.day);
+  clk.print("日");
+  clk.pushSprite(2, 7); // 把画布推送到屏幕的(x,y)位置进行显示
+  clk.deleteSprite();   // 删除画布
+
+  // 显示星期
+  clk.createSprite(68, 32);    // 创建一个画布，画布大小x*y
+  clk.fillSprite(TFT_BLACK);   // 画布颜色
+  clk.setTextColor(TFT_WHITE); // 字体颜色
+  // clk.loadFont(KaiTI_30);
+  clk.setTextDatum(TL_DATUM); // 设置文本显示基准：左上对齐(默认对齐方式)
+  clk.print(Week_str());
+  clk.pushSprite(173, 7); // 把画布推送到屏幕的(x,y)位置进行显示
+  clk.unloadFont();       // 卸载字体
+  clk.deleteSprite();     // 删除画布
 }
 
 void setup()
